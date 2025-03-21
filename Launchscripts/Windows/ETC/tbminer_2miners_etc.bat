8268a1358d9667b90fa9c56b5df14023998de576
Thomas Ryste <thomas.ryste@gmail.com>
2025-03-21 15:17:01 +0100

    Update tbminer_2miners_etc.bat

--- Launchscripts/Windows/ETC/tbminer_2miners_etc.bat
+++ Launchscripts/Windows/ETC/tbminer_2miners_etc.bat
@@ -1,4 +1,4 @@
-Set ETC_WALLET=0xe055f3f9865c8cee4747eeefaab6f0765ef7dace
+Set ETC_WALLET=0xf7b4ff31422206ec4eb427e69d27a9b93fac0c8c
 
 :loop
 	TBMiner --algo etchash --hostname etc.2miners.com --port 1010 --wallet %ETC_WALLET% --worker-name %computername%
